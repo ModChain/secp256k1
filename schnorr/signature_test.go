d02366683f2fd580d5486847b7371c005f90b8eb
Mark Karpeles <magicaltux@gmail.com>
2024-02-20 00:43:38 +0900

    fix test imports

--- schnorr/signature_test.go
+++ schnorr/signature_test.go
@@ -11,8 +11,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/decred/dcrd/crypto/blake256"
-	"github.com/decred/dcrd/dcrec/secp256k1/v4"
+	"github.com/ModChain/blake256"
+	"github.com/ModChain/secp256k1"
 )
 
 // TestSignatureParsing ensures that signatures are properly parsed including
