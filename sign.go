22edcea31e5d045aa48e14d7025cd1db16453c4e
Mark Karpeles <magicaltux@gmail.com>
2024-10-13 02:26:16 +0900

    add format to signature options

--- sign.go
+++ sign.go
@@ -5,8 +5,16 @@ import (
 	"io"
 )
 
+type SignatureFormat uint
+
+const (
+	SignFormatDER SignatureFormat = iota // DER format by default
+	SignFormatCompact
+)
+
 type SignOptions struct {
-	Hash crypto.Hash
+	Format SignatureFormat
+	Hash   crypto.Hash
 }
 
 func (s *SignOptions) HashFunc() crypto.Hash {
@@ -16,6 +24,25 @@ func (s *SignOptions) HashFunc() crypto.Hash {
 // Sign will sign the provided digest, returning the resulting signature. [SignOptions] can be used
 // to pass options.
 func (privkey *PrivateKey) Sign(rand io.Reader, digest []byte, opts crypto.SignerOpts) ([]byte, error) {
-	sign := Sign(privkey, digest)
-	return sign.Serialize(), nil // DER
+	var opt *SignOptions
+	if o, ok := opts.(*SignOptions); ok {
+		opt = o
+	} else {
+		opt = &SignOptions{}
+	}
+
+	sig, pubKeyRecoveryCode := signRFC6979(privkey, digest)
+
+	switch opt.Format {
+	case SignFormatCompact:
+		var b [65]byte
+		b[0] = pubKeyRecoveryCode
+		sig.r.PutBytesUnchecked(b[1:33])
+		sig.s.PutBytesUnchecked(b[33:65])
+		return b[:], nil
+	case SignFormatDER:
+		fallthrough
+	default:
+		return sig.Serialize(), nil // DER
+	}
 }
